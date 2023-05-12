dc4bd06ea27ed9760d113cbc9f0656bbc7d102e8
Zilehuda Tariq <zilehuda.tariq@gmail.com>
2023-05-12 06:44:57 +0500

    write all tests for orders

--- tests/orders_test.go
+++ tests/orders_test.go
@@ -42,3 +42,102 @@ func TestAddOrder(t *testing.T) {
 	assert.Equal(t, 123, order.OrderID)
 	assert.Equal(t, "ABC123", order.OrderNumber)
 }
+
+func TestGetOrder(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Verify the request parameters if needed
+		assert.Equal(t, "/orders/123", r.URL.Path)
+		assert.Equal(t, "GET", r.Method)
+
+		// Return a sample response
+		order := &shipstation.Order{OrderID: 123, OrderNumber: "ABC123"}
+		responseData, _ := json.Marshal(order)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseData)
+	}))
+	defer server.Close()
+
+	// Create a new instance of the ShipStation client
+	client := shipstation.NewShipStation("your-api-key", "your-api-secret")
+	client.SetBaseURL(server.URL)
+
+	// Call the GetOrder method
+	order, err := client.GetOrder(123)
+
+	// Assert the expected behavior
+	assert.NoError(t, err)
+	assert.NotNil(t, order)
+	assert.Equal(t, 123, order.OrderID)
+	assert.Equal(t, "ABC123", order.OrderNumber)
+}
+
+func TestGetOrders(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Verify the request parameters if needed
+		assert.Equal(t, "/orders", r.URL.Path)
+		assert.Equal(t, "GET", r.Method)
+
+		// Return a sample response
+		orderResponse := shipstation.OrdersResponse{
+			Page:  1,
+			Total: 1,
+			Orders: []shipstation.Order{
+				{OrderID: 123, OrderNumber: "ABC123"},
+				{OrderID: 456, OrderNumber: "DEF456"},
+			},
+		}
+
+		responseData, _ := json.Marshal(orderResponse)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseData)
+	}))
+	defer server.Close()
+
+	// Create a new instance of the ShipStation client
+	client := shipstation.NewShipStation("your-api-key", "your-api-secret")
+	client.SetBaseURL(server.URL)
+
+	// Call the GetOrders method
+	ordersResponse, err := client.GetOrders()
+
+	// Assert the expected behavior
+	assert.NoError(t, err)
+	assert.NotNil(t, ordersResponse)
+	assert.Len(t, ordersResponse.Orders, 2)
+	assert.Equal(t, 123, ordersResponse.Orders[0].OrderID)
+	assert.Equal(t, "ABC123", ordersResponse.Orders[0].OrderNumber)
+	assert.Equal(t, 456, ordersResponse.Orders[1].OrderID)
+	assert.Equal(t, "DEF456", ordersResponse.Orders[1].OrderNumber)
+}
+
+func TestDeleteOrder(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Verify the request parameters if needed
+		assert.Equal(t, "/orders/123", r.URL.Path)
+		assert.Equal(t, "DELETE", r.Method)
+
+		// Return a sample response
+		responseData := []byte(`{"message": "Order deleted successfully"}`)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(responseData)
+	}))
+	defer server.Close()
+
+	// Create a new instance of the ShipStation client
+	client := shipstation.NewShipStation("your-api-key", "your-api-secret")
+	client.SetBaseURL(server.URL)
+
+	// Call the DeleteOrder method
+	deleteResponse, err := client.DeleteOrder(123)
+
+	// Assert the expected behavior
+	assert.NoError(t, err)
+	assert.NotNil(t, deleteResponse)
+	assert.Equal(t, "Order deleted successfully", deleteResponse.Message)
+}
